d540af63864eda846f353fc5bf9bc8a1a35ffbd5
Kyle F. Butts <buttskyle96@gmail.com>
2021-05-24 16:46:52 -0600

    Remove Autor JOLE data

--- data-raw/gen_data.R
+++ data-raw/gen_data.R
@@ -8,11 +8,6 @@ df_hom <- gen_data(panel = c(1990, 2020), g1 = 2000, g2 = 2010, g3 = 0, te1 = 2,
 df_het <- gen_data(panel = c(1990, 2020), g1 = 2000, g2 = 2010, g3 = 0, te1 = 2, te2 = 1, te3 = 0, te_m1 = 0.05, te_m2 = 0.15, te_m3 = 0)
 
 
-# Autor JOLE -------------------------------------------------------------------
-
-autor <- haven::read_dta("data-raw/autor-jole-2003.dta")
-
 # Export Data ------------------------------------------------------------------
 usethis::use_data(df_hom, overwrite = TRUE)
 usethis::use_data(df_het, overwrite = TRUE)
-usethis::use_data(autor, overwrite = TRUE)
