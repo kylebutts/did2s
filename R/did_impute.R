ad5d56972de3cde948743045dd3d6d42b4462e15
Kyle F. Butts <buttskyle96@gmail.com>
2021-05-29 17:04:18 -0600

    Fixed Z in did_impute

--- R/did_impute.R
+++ R/did_impute.R
@@ -1,4 +1,5 @@
-did_impute <- function(data, yname, group_name, rel_time_name, id_name, first_stage_formula, treat_var, weights){
+#' Treatment effect estimation and pre-trend testing in staggered adoption diff-in-diff designs with an imputation approach of Borusyak, Jaravel, and Spiess (2021)
+did_impute <- function(data, yname, gname, tname, idname, first_stage_formula, treat_var, weights){
 	# Treat
 	treat <- data[data[[treat_var]] == 1,]
 
@@ -17,28 +18,28 @@ did_impute <- function(data, yname, group_name, rel_time_name, id_name, first_st
 	}
 
 
-
-	# Equation (6) of Borusyak et. al. 2021
-	# Calculate v_it^*
-
 	# Create Zs
 	Z_0 <- create_Z(0)
 	Z_1 <- create_Z(1)
 	Z <- create_Z(c(0,1))
+	v_proj <- - Z %*% solve(t(Z_0) %*% Z_0) %*% t(Z_1)
 
 	se <- c()
 	for(weight in weights) {
 
-		# Not Working
-		# v <- - t(Z) %*% solve(t(Z_0) %*% Z_0) %*% t(Z_1) %*% w
-		w <- data[data[[treat_var]] == 1,][[weight]]
-		data$zz000v <- data[[weight]]
+		# Equation (6) of Borusyak et. al. 2021
+		# Calculate v_it^*
+		data$zz000v <- v_proj %*% w
 
 		# Equation (10) of Borusyak et. al. 2021
 		# Calculate tau_it - \bar{\tau}_{et}
-
 		data <- data %>%
-			dplyr::group_by(!!rlang::sym(group_name), !!rlang::sym(rel_year_name))  %>%
+			# Create event time
+			dplyr::mutate(
+				zz000event_time = !!rlang::sym(tname) - !!rlang::sym(gname)
+			)
+			# group_by Event Group and Event Time
+			dplyr::group_by(!!rlang::sym(gname), zz000event_time)  %>%
 			dplyr::mutate(
 				zz000tau_et = if_else(!!rlang::sym(treat_var) == 1,
 									  sum(zz000v^2 * zz000adj)/sum(zz000v^2) * treat,
@@ -52,7 +53,7 @@ did_impute <- function(data, yname, group_name, rel_time_name, id_name, first_st
 		variance <- data %>%
 			dplyr::group_by(!!rlang::sym(id_name)) %>%
 			dplyr::summarize(zz000temp = sum(zz000v * zz000tau_centered)^2) %>%
-			pull(zz000temp) %>%
+			dplyr::pull(zz000temp) %>%
 			mean()
 
 		se <- c(se, sqrt(variance))
@@ -60,8 +61,6 @@ did_impute <- function(data, yname, group_name, rel_time_name, id_name, first_st
 
 	return(list(estimate = est, se = sqrt(variance)))
 }
-
-
 
 create_Z <- function(treat_val) {
 	data <- dplyr::filter(data, !!rlang::sym(treat_var) == treat_val)
@@ -76,11 +75,7 @@ create_Z <- function(treat_val) {
 			var <- stringr::str_extract(x, ".*(?=::)")
 			val <- stringr::str_extract(x, "(?<=::).*")
 
-			if(inherits(data[[var]], "numeric")){
-				return(data[[var]] == val)
-			} else {
-				return(data[[var]] == 'val')
-			}
+			return(data[[var]] == val)
 		}
 		# covariates
 		else {
@@ -95,15 +90,16 @@ create_Z <- function(treat_val) {
 
 
 
-data("df_hom")
-
-# weight = 1/n
-data = df_hom %>% group_by(treat) %>% mutate(weight = 1/n() * treat)
-yname = "dep_var"
-group_name = "group"
-rel_year_name = "rel_year"
-id_name = "unit"
-first_stage_formula = "i(state) + i(year)"
-treat_var = "treat"
-weights = "weight"
+## Testing ---------------------------------------------------------------------
+# data("df_hom")
+#
+# # weight = 1/n
+# data = df_hom %>% group_by(treat) %>% mutate(weight = 1/n() * treat)
+# yname = "dep_var"
+# group_name = "group"
+# rel_year_name = "rel_year"
+# id_name = "unit"
+# first_stage_formula = "i(state) + i(year)"
+# treat_var = "treat"
+# weights = "weight"
 
