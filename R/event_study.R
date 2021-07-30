c33b5ebc31f33031374ac8abfa20278dbfbea082
Kyle F. Butts <buttskyle96@gmail.com>
2021-07-30 11:38:59 -0600

    Depiped event-study

--- R/event_study.R
+++ R/event_study.R
@@ -25,14 +25,11 @@ event_study = function(data, yname, idname, gname, tname, xformla = NULL, horizo
 	data[is.na(data[[gname]]), gname] = 0
 
 	# Create event time
-	data = data %>% dplyr::mutate(
-		zz000event_time = dplyr::if_else(
-			is.na(!!rlang::sym(gname)) | !!rlang::sym(gname) == 0,
-			-Inf,
-			as.numeric(!!rlang::sym(tname) - !!rlang::sym(gname))
-		)
+	data$zz000event_time = ifelse(
+		is.na(data[[gname]]) | data[[gname]] == 0 | data[[gname]] == Inf,
+		-Inf,
+		as.numeric(data[[tname]] - data[[gname]])
 	)
-
 
 	event_time = unique(data$zz000event_time)
 	event_time = event_time[!is.na(event_time) & is.finite(event_time)]
@@ -56,14 +53,21 @@ event_study = function(data, yname, idname, gname, tname, xformla = NULL, horizo
 		twfe_formula = stats::as.formula(glue::glue("{yname} ~ 1 + {xformla_null} + i(zz000event_time, ref = -1) | {idname} + {tname}"))
 		est_twfe = fixest::feols(twfe_formula, data = data, warn = F, notes = F)
 
-		tidy_twfe = broom::tidy(est_twfe) %>%
-			dplyr::filter(stringr::str_detect(term, "zz000event_time::")) %>%
-			dplyr::mutate(
-				term = stringr::str_replace(term, "zz000event_time::", ""),
-				term = as.numeric(term)
-			) %>%
-			dplyr::select(term, estimate, std.error) %>%
-			dplyr::mutate(estimator = "TWFE")
+		# Extract coefficients and standard errors
+		tidy_twfe = broom::tidy(est_twfe)
+
+		# Extract zz000event_time
+		tidy_twfe = tidy_twfe[stringr::str_detect(tidy_twfe$term, "zz000event_time::"), ]
+
+		# Make event time into a numeric
+		tidy_twfe$term = stringr::str_replace(tidy_twfe$term, "zz000event_time::", "")
+		tidy_twfe$term = as.numeric(tidy_twfe$term)
+
+		# Subset column
+		tidy_twfe = tidy_twfe[, c("term", "estimate", "std.error")]
+
+		# Add estimator column
+		tidy_twfe$estimator = "TWFE"
 	})
 
 	if(is.null(tidy_twfe)) cli::cli_warn("TWFE Failed")
@@ -79,14 +83,21 @@ event_study = function(data, yname, idname, gname, tname, xformla = NULL, horizo
 
 		est_did2s = did2s::did2s(data, yname = yname, first_stage = did2s_first_stage, second_stage = ~i(zz000event_time, ref=-Inf), treatment = "zz000treat", cluster_var = idname, verbose = FALSE)
 
-		tidy_did2s = broom::tidy(est_did2s) %>%
-			dplyr::filter(stringr::str_detect(term, "zz000event_time::")) %>%
-			dplyr::mutate(
-				term = stringr::str_replace(term, "zz000event_time::", ""),
-				term = as.numeric(term)
-			) %>%
-			dplyr::select(term, estimate, std.error) %>%
-			dplyr::mutate(estimator = "Gardner (2021)")
+		# Extract coefficients and standard errors
+		tidy_did2s = broom::tidy(est_did2s)
+
+		# Extract zz000event_time
+		tidy_did2s = tidy_did2s[stringr::str_detect(tidy_did2s$term, "zz000event_time::"), ]
+
+		# Make event time into a numeric
+		tidy_did2s$term = stringr::str_replace(tidy_did2s$term, "zz000event_time::", "")
+		tidy_did2s$term = as.numeric(tidy_did2s$term)
+
+		# Subset columns
+		tidy_did2s = tidy_did2s[, c("term", "estimate", "std.error")]
+
+		# Add estimator column
+		tidy_did2s$estimator = "Gardner (2021)"
 	})
 
 	if(is.null(tidy_did2s)) cli::cli_warn("Gardner (2021) Failed")
@@ -102,9 +113,14 @@ event_study = function(data, yname, idname, gname, tname, xformla = NULL, horizo
 		est_did = did::att_gt(yname = yname, tname = tname, idname = idname, gname = gname, xformla = xformla, data = data) %>%
 			did::aggte(type = "dynamic", na.rm = TRUE)
 
-		tidy_did = broom::tidy(est_did) %>%
-			dplyr::select(term = event.time, estimate, std.error) %>%
-			dplyr::mutate(estimator = "Callaway and Sant'Anna (2020)")
+		# Extract es coefficients
+		tidy_did = broom::tidy(est_did)
+
+		# Subset columns
+		tidy_did$term = tidy_did$event.time
+		tidy_did = tidy_did[, c("term", "estimate", "std.error")]
+
+		tidy_did$estimator = "Callaway and Sant'Anna (2020)"
 	})
 
 	if(is.null(tidy_did)) cli::cli_warn("Callaway and Sant'Anna (2020) Failed")
@@ -127,14 +143,20 @@ event_study = function(data, yname, idname, gname, tname, xformla = NULL, horizo
 
 		est_sunab = fixest::feols(sunab_formla, data = data)
 
-		tidy_sunab = broom::tidy(est_sunab) %>%
-			filter(stringr::str_detect(term, glue::glue("{tname}::"))) %>%
-			dplyr::mutate(
-				term = stringr::str_replace(term, glue::glue("{tname}::"), ""),
-				term = as.numeric(term)
-			) %>%
-			dplyr::select(term, estimate, std.error) %>%
-			dplyr::mutate(estimator = "Sun and Abraham (2020)")
+		tidy_sunab = broom::tidy(est_sunab)
+
+		# Extract zz000event_time
+		tidy_sunab = tidy_sunab[stringr::str_detect(tidy_sunab$term, glue::glue("{tname}::")), ]
+
+		# Make event time into a numeric
+		tidy_sunab$term = stringr::str_replace(tidy_sunab$term, glue::glue("{tname}::"), "")
+		tidy_sunab$term = as.numeric(tidy_sunab$term)
+
+		# Subset columns
+		tidy_sunab = tidy_sunab[, c("term", "estimate", "std.error")]
+
+		# Add estimator column
+		tidy_sunab$estimator = "Sun and Abraham (2020)"
 	})
 
 	if(is.null(tidy_sunab)) cli::cli_warn("Sun and Abraham (2020) Failed")
@@ -148,11 +170,18 @@ event_study = function(data, yname, idname, gname, tname, xformla = NULL, horizo
 	try({
 		impute_first_stage = stats::as.formula(glue::glue("~ {xformla_null} | {idname} + {tname}"))
 
-		tidy_impute = did2s::did_imputation(data,
+		tidy_impute = didimputation::did_imputation(data,
 									 yname = yname, gname = gname, tname = tname, idname = idname,
-									 first_stage = impute_first_stage, horizon = TRUE, pretrends = TRUE) %>%
-			dplyr::select(term, estimate, std.error) %>%
-			dplyr::mutate(estimator = "Borusyak, Jaravel, Spiess (2021)", term = as.numeric(term))
+									 first_stage = impute_first_stage, horizon = TRUE, pretrends = TRUE)
+
+		# Subset columns
+		tidy_impute = tidy_impute[, c("term", "estimate", "std.error")]
+
+		# Make event time into a numeric
+		tidy_impute$term = as.numeric(tidy_impute$term)
+
+		# Add estimator column
+		tidy_impute$estimator = "Borusyak, Jaravel, Spiess (2021)"
 	})
 
 	if(is.null(tidy_impute)) cli::cli_warn("Borusyak, Jaravel, Spiess (2021) Failed")
@@ -164,14 +193,29 @@ event_study = function(data, yname, idname, gname, tname, xformla = NULL, horizo
 	tidy_staggered = NULL
 
 	try({
+		# Make untreated g = Inf
+		data_staggered = data
+
+		data_staggered[,gname] = ifelse(
+			data_staggered[[gname]] == 0,
+			Inf,
+			data_staggered[[gname]]
+		)
+
 		tidy_staggered = staggered::staggered(
-			data %>%
-				dplyr::mutate(!!rlang::sym(gname) := dplyr::if_else(!!rlang::sym(gname) == 0, Inf, !!rlang::sym(gname))),
+			data_staggered,
 			i = idname, t = tname, g = gname, y = yname, estimand = "eventstudy",
 			eventTime = event_time[is.finite(event_time) & event_time != -1]
-		) %>%
-			dplyr::select(term = eventTime, estimate, std.error = se) %>%
-			dplyr::mutate(estimator = "Roth and Sant'Anna (2021)")
+		)
+
+		# Subset columns
+		tidy_staggered$term = tidy_staggered$eventTime
+		tidy_staggered$std.error = tidy_staggered$se
+
+		tidy_staggered = tidy_staggered[, c("term", "estimate", "std.error")]
+
+		# Add estimator column
+		tidy_staggered$estimator = "Roth and Sant'Anna (2021)"
 	})
 
 	if(is.null(tidy_staggered)) cli::cli_warn("Roth and Sant'Anna (2021) Failed")
@@ -204,33 +248,35 @@ plot_event_study = function(out, seperate = TRUE, horizon = NULL) {
 	levels = c("TWFE", "Borusyak, Jaravel, Spiess (2021)", "Callaway and Sant'Anna (2020)", "Gardner (2021)", "Roth and Sant'Anna (2021)",  "Sun and Abraham (2020)")
 	levels = levels[levels %in% estimators]
 
+	# Make estimator into factor
+	out$estimator = factor(out$estimator, levels = levels)
+
+
 	# Subset color scales
 	color_scale = c("TWFE" = "#374E55", "Gardner (2021)" = "#DF8F44", "Callaway and Sant'Anna (2020)" = "#00A1D5", "Sun and Abraham (2020)" = "#B24745", "Roth and Sant'Anna (2021)" = "#79AF97", "Borusyak, Jaravel, Spiess (2021)" = "#6A6599")
 	color_scale = color_scale[names(color_scale) %in% estimators]
 
+
 	# create confidence intervals
-	out = out %>%
-		dplyr::mutate(
-			ci_lower = estimate - 1.96 * std.error,
-			ci_upper = estimate + 1.96 * std.error,
-			estimator = factor(estimator, levels = levels)
-		)
+	out$ci_lower = out$estimate - 1.96 * out$std.error
+	out$ci_upper = out$estimate + 1.96 * out$std.error
+
+
 
 	# position depending on sepreate
-	if(seperate) position = "identity" else position = position_dodge(width = 0.5)
+	if(seperate) position = "identity" else position = ggplot2::position_dodge(width = 0.5)
 
 	# Subset plot if horizon is specified
 	if(!is.null(horizon)) {
-		out = out %>%
-			dplyr::filter(term >= horizon[1] & term <= horizon[2])
+		out = out[out$term >= horizon[1] & out$term <= horizon[2], ]
 	}
 
 	# max and min of limits
 	y_lims = c(min(out$ci_lower), max(out$ci_upper)) * 1.05
 	x_lims = c(min(out$term) - 1, max(out$term) + 1)
 
-	ggplot2::ggplot(out, ggplot2::aes(x = term, y = estimate, color = estimator, ymin = ci_lower, ymax = ci_upper)) +
-		{ if(seperate) ggplot2::facet_wrap(~ estimator, scales="free") } +
+	ggplot2::ggplot(ggplot2::aes(x = out$term, y = out$estimate, color = out$estimator, ymin = out$ci_lower, ymax = out$ci_upper)) +
+		{ if(seperate) ggplot2::facet_wrap(~ out$estimator, scales="free") } +
 		ggplot2::geom_point(position = position) +
 		ggplot2::geom_errorbar(position = position) +
 		ggplot2::geom_vline(xintercept = -0.5, linetype = "dashed") +
